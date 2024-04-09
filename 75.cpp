8e09b63ec9a25fc619c131f57aeaec3edeb9c69f
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2024-04-10 00:08:48 +0530

    Create 75.cpp

---
+++ 75.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int s = 0, m = 0, e = nums.size() - 1;
+        while (m <= e) {
+            if (nums[m] == 0) {
+                swap(nums[m],nums[s]);
+                m++;
+                s++;
+            }
+            else if (nums[m] == 1) {
+                m++;
+            }
+            else {
+                swap(nums[m], nums[e]);
+                e--;
+            }                
+        }
+    }
+};
