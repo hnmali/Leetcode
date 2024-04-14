5490b32ac983e8d4264df2c2ff478a5cdc0fb3d3
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2024-04-14 14:10:46 +0530

    Create 2022.cpp

---
+++ 2022.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        vector<vector<int>> ans;
+        if (m * n != original.size())
+            return ans;
+        int k = 0;
+        for (int i = 0; i < m; i++) {
+            vector<int> temp;
+            for (int j = 0; j < n; j++) {
+                temp.push_back(original[k]);
+                k++;
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
