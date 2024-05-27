f997987d5b18dd761d58e83e40a92be286c0ec08
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2024-05-27 14:37:10 +0530

    Create 2485.cpp

---
+++ 2485.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int totalSum = n * (n + 1) / 2;
+        int currSum = 0;
+        for (int i = 1; i <= n; i++) {
+            currSum += i;
+            if (currSum == totalSum) 
+                return i;
+            totalSum -= i;
+        }
+        return -1;
+    }
+};
