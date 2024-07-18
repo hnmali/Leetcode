b7afa2c196cfbb71ec0a33bd7fe990ec5530eb3c
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2024-07-18 16:10:28 +0530

    Create 2125.cpp

---
+++ 2125.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int ans = 0;
+        vector<int> lasers;
+        for (int i = 0; i < bank.size(); i++) {
+            int temp = 0;
+            for (int j = 0; j < bank[i].size(); j++) {
+                if (bank[i][j] == '1')
+                    temp++;
+            }
+        if (temp)
+            lasers.push_back(temp);
+        }
+        for (int i = 0; !lasers.empty() && i < lasers.size()-1; i++)
+            ans = ans + (lasers[i]*lasers[i+1]);
+        return ans;
+    }
+};
