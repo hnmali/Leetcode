21b1530bd6977deaef77c368f7ecbdaa715819f9
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2024-07-29 18:27:27 +0530

    Create 1561.cpp

---
+++ 1561.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& piles) {
+        int ans = 0, n = piles.size();
+        sort(piles.begin(), piles.end());
+        for (int i = n-2; i >= n/3; i -= 2)
+            ans += piles[i];        
+        return ans;
+    }
+};
