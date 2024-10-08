b85a68715d240f29110a063ddb6e44f44219aa84
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2024-10-08 22:54:18 +0530

    Update 73.cpp

--- 73.cpp
+++ 73.cpp
@@ -18,3 +18,41 @@ public:
             update(matrix, index[i].first, index[i].second);
     }
 };
+
+//Optimized solution
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        bool flag = false;
+        for (int i = 0; i < matrix.size(); i++) {
+            if (matrix[i][0] == 0)
+                flag = true;
+        }
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    if(j == 0)
+                        continue;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        if (matrix.size() > 1) {
+            for (int i = 1; i < matrix.size(); i++) {
+                for (int j = 1; j < matrix[0].size(); j++) {
+                    if (matrix[i][0] == 0 or matrix[0][j] == 0)
+                        matrix[i][j] = 0;
+                }
+            }
+        }
+        if (matrix[0][0] == 0) {
+            for (int i = 0; i < matrix[0].size(); i++) 
+                matrix[0][i] = 0;
+        }
+        if (flag) {
+            for (int i = 0; i < matrix.size(); i++) 
+                matrix[i][0] = 0;
+        }
+    }
+};
