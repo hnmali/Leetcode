3f651dd865f79cbffb151373889b9ffecd21eb47
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2025-04-04 21:40:00 +0530

    Create 594.cpp

---
+++ 594.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        int res = 0;
+        for (int num : nums) 
+            freq[num]++;
+        for (auto pair : freq) 
+            if (freq.find(pair.first + 1) != freq.end()) 
+                res = max(res, pair.second + freq[pair.first+1]);
+        return res;
+    }
+};
