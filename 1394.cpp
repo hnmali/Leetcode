1f7b11f944f8ed05ba8069295246d289b2e7cb53
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2025-07-05 19:21:33 +0530

    Create 1394.cpp

---
+++ 1394.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        unordered_map<int, int> mp;
+        int ans = -1;
+        for(int i = 0; i < arr.size(); i++)
+            mp[arr[i]]++;
+        for(auto it : mp)
+            if(it.first == it.second)
+                ans = max(ans, it.first);
+        return ans;
+    }
+};
