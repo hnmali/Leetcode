9157d0dbc142f3f6724dd8819e821976e6e70a19
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2025-08-03 21:24:42 +0530

    Create 19.cpp

---
+++ 19.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *first = head, *second = head;
+        for (int i = 0; i < n; i++) {
+            if (first == NULL)
+                return head; 
+            first = first->next;
+        }
+        if (first == NULL) {
+            ListNode* temp = head;
+            head = head->next;
+            delete temp;
+            return head;
+        }
+        while (first->next != NULL) {
+            first = first->next;
+            second = second->next;
+        }
+        ListNode* temp = second->next;
+        second->next = second->next->next;
+        delete temp;
+        return head;
+    }
+};
