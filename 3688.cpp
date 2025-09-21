2895abfaf8a61b08d29459897fb8b7fb9de21825
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2025-09-21 16:41:28 +0530

    Create 3688.cpp

---
+++ 3688.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int evenNumberBitwiseORs(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 0; i < nums.size(); i++) 
+            if(!(nums[i]&1))
+                ans |= nums[i];
+        return ans;
+    }
+};
