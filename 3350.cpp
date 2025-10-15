86a37234e6da9708fe21e83b1fb2e7c6649951b4
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2025-10-15 18:39:24 +0530

    Create 3350.cpp

---
+++ 3350.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxIncreasingSubarrays(vector<int>& nums) {
+        int cnt = 1, precnt = 0, ans = 0;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] > nums[i - 1]) 
+                ++cnt;
+            else {
+                precnt = cnt;
+                cnt = 1;
+            }
+            ans = max(ans, min(precnt, cnt));
+            ans = max(ans, cnt / 2);
+        }
+        return ans;
+    }
+};
