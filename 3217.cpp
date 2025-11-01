70fd4a6a57904b63ccb3527a6986ab919a9701a5
Himanshu Mali <149325068+hnmali@users.noreply.github.com>
2025-11-01 17:16:09 +0530

    Create 3217.cpp

---
+++ 3217.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int> st;
+        for(int i = 0; i < nums.size(); i++)    
+            st.insert(nums[i]);
+        while (head != NULL && st.count(head->val) > 0) {
+            ListNode* temp = head;
+            head = head->next;
+        }
+        if (head == NULL) 
+            return NULL;        
+        ListNode* current = head;
+        while (current->next != NULL) {
+            if (st.contains(current->next->val)) {
+                ListNode* temp = current->next;
+                current->next = current->next->next;
+            } 
+            else 
+                current = current->next;
+        }
+        return head;
+    }
+};
